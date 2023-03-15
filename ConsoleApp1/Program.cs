36d92a46b35786efe8b4c6a20812ee0da3c2114e
rodrigo462003 <rodrigo46.2003@gmail.com>
2023-03-15 12:07:34 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+AL76651.Apresentacao();
+
+public class AL76651 {
+	public static void Apresentacao(){
+			Console.WriteLine("AL76651");
+		}
+}
+
+
